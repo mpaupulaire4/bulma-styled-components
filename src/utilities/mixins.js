d4d733f7c682285007ca58ee0ad21a2df88afcc1
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-06-09 15:50:13 -0600

    finished mixins

--- src/utilities/mixins.js
+++ src/utilities/mixins.js
@@ -1,9 +1,9 @@
 import { css } from 'styled-components'
 import { clearFix as ClearFix } from 'polished'
-import getVariables from './derived-variables'
+import { spinAround } from './animations'
 
-function fromTheme (key) {
-  return (props) => props.theme[key]
+function fromTheme(key) {
+  return props => props.theme[key]
 }
 
 export const clearFix = css`
@@ -14,14 +14,14 @@ export function center(width, height = 0) {
   if (height) {
     return css`
       position: absolute;
-      left: calc(50% - (${$width} / 2));
-      top: calc(50% - (${$height} / 2));
+      left: calc(50% - (${width} / 2));
+      top: calc(50% - (${height} / 2));
     `
   }
   return css`
     position: absolute;
-    left: calc(50% - (${$width} / 2));
-    top: calc(50% - (${$width} / 2));
+    left: calc(50% - (${width} / 2));
+    top: calc(50% - (${width} / 2));
   `
 }
 
@@ -57,15 +57,15 @@ export function hamburger(dimensions) {
       width: 16px;
       &:nth-child(1) {
         top: calc(50% - 6px);
-        ${props => props.active ? 'transform: translateY(5px) rotate(45deg);' : ''}
+        ${props => props.active ? 'transform: translateY(5px) rotate(45deg);' : ''/* eslint-disable-line no-confusing-arrow */}
       }
       &:nth-child(2) {
         top: calc(50% - 1px);
-        ${props => props.active ? 'opacity: 0;' : ''}
+        ${props => props.active ? 'opacity: 0;' : ''/* eslint-disable-line no-confusing-arrow */}
       }
       &:nth-child(3) {
         top: calc(50% + 4px);
-        ${props => props.active ? 'transform: translateY(-5px) rotate(-45deg);' : ''}
+        ${props => props.active ? 'transform: translateY(-5px) rotate(-45deg);' : ''/* eslint-disable-line no-confusing-arrow */}
       }
     }
     &:hover {
@@ -77,8 +77,6 @@ export function hamburger(dimensions) {
 export const overflow_touch = css`
   -webkit-overflow-scrolling: touch;
 `
-
-const placeholders = [':-moz', ':-webkit-input', '-moz', '-ms-input']
 
 export const placeholder = (...args) => {
   const content = css(...args)
@@ -100,128 +98,133 @@ export const placeholder = (...args) => {
 
 // Responsiveness
 
-export const from = (device) => (...args) => css`
-  @media screen and (min-width: ${device}) {
+export const from = deviceType => (...args) => css`
+  @media screen and (min-width: ${deviceType}) {
     ${css(...args)}
   }
 `
 
-export const untill = (device) => (...args) => css`
-  @media screen and (max-width: $device - 1px) {
+export const untill = deviceType => (...args) => css`
+  @media screen and (max-width: ${deviceType} - 1px) {
     ${css(...args)}
   }
 `
 
 export const mobile = (...args) => css`
-  @media screen and (max-width: ${props => props.theme.tablet} - 1px) {
+  @media screen and (max-width: ${fromTheme('tablet')} - 1px) {
     ${css(...args)}
   }
 `
 export const tablet = (...args) => css`
-  @media screen and (min-width: ${props => props.theme.tablet}), print {
+  @media screen and (min-width: ${fromTheme('tablet')}), print {
     ${css(...args)}
   }
 `
 
 export const tablet_only = (...args) => css`
-  @media screen and (min-width: ${props => props.theme.tablet}) and (max-width: ${props => props.theme.desktop} - 1px) {
+  @media screen and (min-width: ${fromTheme('tablet')}) and (max-width: ${fromTheme('desktop')} - 1px) {
     ${css(...args)}
   }
 `
 
-export const touch = ({desktop} = getVariables()) => (...args) => css`
-  @media screen and (max-width: ${desktop} - 1px) {
+export const touch = (...args) => css`
+  @media screen and (max-width: ${fromTheme('desktop')} - 1px) {
     ${css(...args)}
   }
 `
 
-export const desktop = ({desktop} = getVariables()) => (...args) => css`
-  @media screen and (min-width: ${desktop}) {
+export const desktop = (...args) => css`
+  @media screen and (min-width: ${fromTheme('desktop')}) {
     ${css(...args)}
   }
 `
 
-export const desktop_only = ({
-  desktop,
-  widescreen,
-  'widescreen-enabled': enabled,
-} = getVariables()) => enabled ? (...args) => {
-  return css`
-    @media screen and (min-width: ${desktop}) and (max-width: ${widescreen} - 1px) {
-      ${css(...args)}
+export const desktop_only = (...args) => css`
+  ${(props) => {
+    if (props.theme['widescreen-enabled']) {
+      return css`
+        @media screen and (min-width: ${props.theme['desktop']}) and (max-width: ${props.theme['widescreen']} - 1px) {
+          ${css(...args)}
+        }
+      ` /* eslint-disable-line */
     }
-  `
-} : ''
+    return ''
+  }}
+`
 
-export const until_widescreen = ({
-  widescreen,
-  'widescreen-enabled': enabled,
-} = getVariables()) => enabled ? (...args) => {
-  return css`
-    @media screen and (max-width: ${widescreen} - 1px) {
-      ${css(...args)}
+export const until_widescreen = (...args) => css`
+  ${(props) => {
+    if (props.theme['widescreen-enabled']) {
+      return css`
+        @media screen and (max-width: ${props.theme['widescreen']} - 1px) {
+          ${css(...args)}
+        }
+      ` /* eslint-disable-line */
     }
-  `
-} : ''
+    return ''
+  }}
+`
 
-export const widescreen = ({
-  desktop,
-  widescreen,
-  'widescreen-enabled': enabled,
-} = getVariables()) => enabled ? (...args) => {
-  return css`
-    @media screen and (min-width: ${widescreen}) {
-      ${css(...args)}
+export const widescreen = (...args) => css`
+  ${(props) => {
+    if (props.theme['widescreen-enabled']) {
+      return css`
+        @media screen and (min-width: ${props.theme['widescreen']}) {
+          ${css(...args)}
+        }
+      ` /* eslint-disable-line */
     }
-  `
-} : ''
+    return ''
+  }}
+`
 
-export const widescreen_only = ({
-  fullhd,
-  widescreen,
-  'widescreen-enabled': enabled1,
-  'fullhd-enabled': enabled2,
-} = getVariables()) => enabled1 && enabled2 ? (...args) => {
-  return css`
-    @media screen and (min-width: ${widescreen}) and (max-width: ${fullhd} - 1px) {
-      ${css(...args)}
+export const widescreen_only = (...args) => css`
+  ${(props) => {
+    if (props.theme['fullhd-enabled'] && props.theme['widescreen-enabled']) {
+      return css`
+        @media screen and (min-width: ${props.theme['widescreen']}) and (max-width: ${props.theme['fullhd']} - 1px) {
+          ${css(...args)}
+        }
+      ` /* eslint-disable-line */
     }
-  `
-} : ''
+    return ''
+  }}
+`
 
-export const until_fullhd = ({
-  fullhd,
-  'fullhd-enabled': enabled,
-} = getVariables()) => enabled ? (...args) => {
-  return css`
-    @media screen and (max-width: ${fullhd} - 1px) {
-      ${css(...args)}
+export const until_fullhd = (...args) => css`
+  ${(props) => {
+    if (props.theme['fullhd-enabled']) {
+      return css`
+        @media screen and (max-width: ${props.theme['fullhd']} - 1px) {
+          ${css(...args)}
+        }
+      ` /* eslint-disable-line */
     }
-  `
-} : ''
+    return ''
+  }}
+`
 
-export const fullhd = ({
-  fullhd,
-  'fullhd-enabled': enabled,
-} = getVariables()) => enabled ? (...args) => {
-  return css`
-    @media screen and (min-width: ${fullhd}) {
-      ${css(...args)}
+export const fullhd = (...args) => css`
+  ${(props) => {
+    if (props.theme['fullhd-enabled']) {
+      return css`
+        @media screen and (min-width: ${props.theme['fullhd']}) {
+          ${css(...args)}
+        }
+      ` /* eslint-disable-line */
     }
-  `
-} : ''
+    return ''
+  }}
+`
 
 // Placeholders
 
 export const unselectable = css`
   -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
   user-select: none;
 `
 
-const arrow = css`
+export const arrow = color => css`
   border: 3px solid transparent;
   border-radius: 2px;
   border-right: 0;
@@ -236,10 +239,6 @@ const arrow = css`
   transform: rotate(-45deg);
   transform-origin: center;
   width: 0.625em;
-`
-
-export const arrow = (color) => css`
-  ${arrow}
   border-color: ${color};
 `
 
@@ -249,13 +248,12 @@ export const block = css`
   }
 `
 
-export const delete_ = css`
+export const Delete = css`
   ${unselectable}
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  background-color: rgba($black, 0.2);
+  appearance: none;
+  background-color: rgba( ${fromTheme('black')} , 0.2);
   border: none;
-  border-radius: ${props => props.theme[radius-rounded]};
+  border-radius: ${fromTheme('radius-rounded')};
   cursor: pointer;
   pointer-events: auto;
   display: inline-block;
@@ -273,7 +271,7 @@ export const delete_ = css`
   width: 20px;
   &::before,
   &::after {
-    background-color: $white;
+    background-color: ${fromTheme('white')};
     content: "";
     display: block;
     left: 50%;
@@ -292,42 +290,56 @@ export const delete_ = css`
   }
   &:hover,
   &:focus {
-    background-color: rgba($black, 0.3);
+    background-color: rgba( ${fromTheme('black')}, 0.3);
   }
   &:active {
-    background-color: rgba($black, 0.4);
+    background-color: rgba( ${fromTheme('black')}, 0.4);
   }
-  // Sizes
-  &.is-small
-    height: 16px;
-    max-height: 16px;
-    max-width: 16px;
-    min-height: 16px;
-    min-width: 16px;
-    width: 16px;
-  &.is-medium
-    height: 24px;
-    max-height: 24px;
-    max-width: 24px;
-    min-height: 24px;
-    min-width: 24px;
-    width: 24px;
-  &.is-large
-    height: 32px;
-    max-height: 32px;
-    max-width: 32px;
-    min-height: 32px;
-    min-width: 32px;
-    width: 32px;
+  ${(props) => {
+    if (props['is-small']) {
+      return `
+        height: 16px;
+        max-height: 16px;
+        max-width: 16px;
+        min-height: 16px;
+        min-width: 16px;
+        width: 16px;
+      `
+    }
+    return ''
+  }}
+  ${(props) => {
+    if (props['is-medium']) {
+      return `
+        height: 24px;
+        max-height: 24px;
+        max-width: 24px;
+        min-height: 24px;
+        min-width: 24px;
+        width: 24px;
+      `
+    }
+    return ''
+  }}
+  ${(props) => {
+    if (props['is-medium']) {
+      return `
+        height: 32px;
+        max-height: 32px;
+        max-width: 32px;
+        min-height: 32px;
+        min-width: 32px;
+        width: 32px;
+      `
+    }
+    return ''
+  }}
 `
 
-export function delete
-  @extend %delete
-
-%loader
-  animation: spinAround 500ms infinite linear;
-  border: 2px solid $border;
-  border-radius: $radius-rounded;
+export const loader = css`
+  animation: ${spinAround} 500ms infinite linear;
+  border: 2px solid ${fromTheme('border')};
+  border-radius: ${fromTheme('radius-rounded')};
   border-right-color: transparent;
   border-top-color: transparent;
   content: "";
@@ -335,22 +347,25 @@ export function delete
   height: 1em;
   position: relative;
   width: 1em;
+`
 
-export function loader
-  @extend %loader
-
-%overlay
-  bottom: 0;
-  left: 0;
-  position: absolute;
-  right: 0;
-  top: 0;
-
-export function overlay($offset: 0);
-  @extend %overlay
-  @if $offset !export function  0
-    bottom: $offset;
-    left: $offset;
-    right: $offset;
-    top: $offset;
+export function overlay(offset = 0) {
+  let extra = ''
+  if (offset) {
+    extra = `
+      bottom: ${offset};
+      left: ${offset};
+      right: ${offset};
+      top: ${offset};
+    `
+  }
+  return css`
+    bottom: 0;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    ${extra}
+  `
+}
 
