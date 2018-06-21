a3e82c5ac7b5fd949b044bcd4dc2ad9492a8b273
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-06-21 14:30:23 -0400

    finishe tags

---
+++ src/elements/__story__/tag.stories.js
@@ -0,0 +1,60 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Tag, Tags } from '../tags'
+import Delete from '../delete'
+
+const Link = Tag.withComponent('a')
+
+const colors = [
+  'black',
+  'dark',
+  'light',
+  'white',
+  'primary',
+  'link',
+  'info',
+  'success',
+  'warning',
+  'danger',
+]
+
+storiesOf('Elements/', module)
+  .add('Tag', () => (
+    <div>
+      <h1>Tag</h1>
+      <Tag>
+        Just a Tag
+      </Tag>
+
+      <h1>As a Link</h1>
+      <Link href="/">
+        Just a Tag
+      </Link>
+
+      <h1>Modifiers</h1>
+      <Tags>
+        <Tag className="is-rounded" >
+          is-rounded
+        </Tag>
+        <Tag className="is-delete" />
+      </Tags>
+      <h1>colors</h1>
+      <Tags>
+        {colors.map(color => (
+          <Tag className={`is-${color}`} >
+            {color}
+          </Tag>
+        ))}
+      </Tags>
+      <h1>With Delete</h1>
+      <Tags>
+        {colors.map(color => (
+          <Tag className={`is-${color}`} >
+            {color}
+            <Delete className="is-small" />
+          </Tag>
+        ))}
+      </Tags>
+    </div>
+  ))
---
+++ src/elements/tags.js
@@ -0,0 +1,158 @@
+/* stylelint-disable no-descending-specificity */
+import styled, { css } from 'styled-components'
+import { darken } from 'polished'
+import Vars from '../utilities/vars'
+import { fromTheme } from '../utilities/functions'
+import Delete from './delete'
+
+
+Vars.addDerivedDefault(vars => ({
+  'tag-background-color': vars['background'],
+  'tag-color': vars['text'],
+  'tag-radius': vars['radius'],
+  'tag-delete-margin': '1px',
+}))
+
+const colorClasses = props => Object.entries(props.theme.colors).reduce((acc, [name, [color, color_invert]]) => css`
+  ${acc}
+  &.is-${name} {
+    background-color: ${color};
+    color: ${color_invert};
+  }
+`, '')
+
+export const Tag = styled.span`
+  align-items: center;
+  background-color: ${fromTheme('tag-background-color')};
+  border-radius: ${fromTheme('tag-radius')};
+  color: ${fromTheme('tag-color')};
+  display: inline-flex;
+  font-size: ${fromTheme('size-small')};
+  height: 2em;
+  justify-content: center;
+  line-height: 1.5;
+  padding-left: 0.75em;
+  padding-right: 0.75em;
+  white-space: nowrap;
+  ${Delete} { /* stylelint-disable-line */
+    margin-left: 0.25rem;
+    margin-right: -0.375rem;
+  }
+
+  /* Colors */
+  ${colorClasses}
+
+  /* Sizes */
+  &.is-medium {
+    font-size: ${fromTheme('size-normal')};
+  }
+  &.is-large {
+    font-size: ${fromTheme('size-medium')};
+  }
+  .icon {
+    &:first-child:not(:last-child) {
+      margin-left: -0.375em;
+      margin-right: 0.1875em;
+    }
+    &:last-child:not(:first-child) {
+      margin-left: 0.1875em;
+      margin-right: -0.375em;
+    }
+    &:first-child:last-child {
+      margin-left: -0.375em;
+      margin-right: -0.375em;
+    }
+  }
+  /* Modifiers */
+  &.is-delete {
+    margin-left: ${fromTheme('tag-delete-margin')};
+    padding: 0;
+    position: relative;
+    width: 2em;
+    &::before,
+    &::after {
+      background-color: currentColor;
+      content: "";
+      display: block;
+      left: 50%;
+      position: absolute;
+      top: 50%;
+      transform: translateX(-50%) translateY(-50%) rotate(45deg);
+      transform-origin: center center;
+    }
+    &::before {
+      height: 1px;
+      width: 50%;
+    }
+    &::after {
+      height: 50%;
+      width: 1px;
+    }
+    &:hover,
+    &:focus {
+      background-color: ${({ theme }) => darken(0.05, theme['tag-background-color'])};
+    }
+    &:active {
+      background-color: ${({ theme }) => darken(0.1, theme['tag-background-color'])};
+    }
+  }
+  &.is-rounded {
+    border-radius: ${fromTheme('radius-rounded')};
+  }
+
+  a& {
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
+
+export const Tags = styled.div`
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  ${Tag} { /* stylelint-disable-line */
+    margin-bottom: 0.5rem;
+    &:not(:last-child) {
+      margin-right: 0.5rem;
+    }
+  }
+  &:last-child {
+    margin-bottom: -0.5rem;
+  }
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+  &.has-addons {
+    ${Tag} { /* stylelint-disable-line */
+      margin-right: 0;
+      &:not(:first-child) {
+        border-bottom-left-radius: 0;
+        border-top-left-radius: 0;
+      }
+      &:not(:last-child) {
+        border-bottom-right-radius: 0;
+        border-top-right-radius: 0;
+      }
+    }
+  }
+  &.is-centered {
+    justify-content: center;
+    ${Tag} { /* stylelint-disable-line */
+      margin-right: 0.25rem;
+      margin-left: 0.25rem;
+    }
+  }
+  &.is-right {
+    justify-content: flex-end;
+    ${Tag} { /* stylelint-disable-line */
+      &:not(:first-child) {
+        margin-left: 0.5rem;
+      }
+      &:not(:last-child) {
+        margin-right: 0;
+      }
+    }
+  }
+`
