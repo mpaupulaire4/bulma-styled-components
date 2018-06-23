98d237019e368165457c08a08077ca3b69737f25
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-06-22 22:32:36 -0400

    card story tweak

--- src/components/__stories__/message.stories.js
+++ src/components/__stories__/message.stories.js
@@ -14,7 +14,14 @@ storiesOf('Components', module)
           <Delete />
         </Message.Header>
         <Message.Body>
-          hey
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla.
+          Vestibulum rhoncus ac ex sit amet fringilla.
+          Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+          Aenean ac <em>eleifend lacus</em>, in mollis lectus.
+          Donec sodales, arcu et sollicitudin porttitor,
+          tortor urna tempor ligula, id porttitor mi magna a neque.
+          Donec dui urna, vehicula et sem eget, facilisis sodales sem.
         </Message.Body>
       </Message>
     </div>
