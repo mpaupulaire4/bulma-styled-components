e8f8a7a04e25fc7238f6efb7f1704c03ddc6486b
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-07-09 13:16:11 -0600

    minor tweaks

--- src/elements/__stories__/box.stories.js
+++ src/elements/__stories__/box.stories.js
@@ -3,16 +3,18 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Box } from '../'
 
-const Link = Box.withComponent('a')
 
 storiesOf('Elements', module)
-  .add('Box', () => (
-    <div>
-      <Box>
-        Just a Box
-      </Box>
-      <Link href="/">
-        Just a Box as a Link
-      </Link>
-    </div>
-  ))
+  .add('Box', () => {
+    const Link = Box.withComponent('a')
+    return (
+      <div>
+        <Box>
+          Just a Box
+        </Box>
+        <Link href="/">
+          Just a Box as a Link
+        </Link>
+      </div>
+    )
+  })
--- src/elements/__stories__/tag.stories.js
+++ src/elements/__stories__/tag.stories.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Tag, Tags, Delete } from '../'
 
-const Link = Tag.withComponent('a')
 
 const colors = [
   'black',
@@ -19,41 +18,44 @@ const colors = [
 ]
 
 storiesOf('Elements', module)
-  .add('Tag', () => (
-    <div>
-      <h1>Tag</h1>
-      <Tag>
-        Just a Tag
-      </Tag>
-
-      <h1>As a Link</h1>
-      <Link href="/">
-        Just a Tag
-      </Link>
-
-      <h1>Modifiers</h1>
-      <Tags>
-        <Tag className="is-rounded" >
-          is-rounded
+  .add('Tag', () => {
+    const Link = Tag.withComponent('a')
+    return (
+      <div>
+        <h1>Tag</h1>
+        <Tag>
+          Just a Tag
         </Tag>
-        <Tag className="is-delete" />
-      </Tags>
-      <h1>colors</h1>
-      <Tags>
-        {colors.map(color => (
-          <Tag key={`${color}-color`} className={`is-${color}`} >
-            {color}
+
+        <h1>As a Link</h1>
+        <Link href="/">
+          Just a Tag
+        </Link>
+
+        <h1>Modifiers</h1>
+        <Tags>
+          <Tag className="is-rounded" >
+            is-rounded
           </Tag>
-        ))}
-      </Tags>
-      <h1>With Delete</h1>
-      <Tags>
-        {colors.map(color => (
-          <Tag key={`${color}-delete`} className={`is-${color}`} >
-            {color}
-            <Delete className="is-small" />
-          </Tag>
-        ))}
-      </Tags>
-    </div>
-  ))
+          <Tag className="is-delete" />
+        </Tags>
+        <h1>colors</h1>
+        <Tags>
+          {colors.map(color => (
+            <Tag key={`${color}-color`} className={`is-${color}`} >
+              {color}
+            </Tag>
+          ))}
+        </Tags>
+        <h1>With Delete</h1>
+        <Tags>
+          {colors.map(color => (
+            <Tag key={`${color}-delete`} className={`is-${color}`} >
+              {color}
+              <Delete className="is-small" />
+            </Tag>
+          ))}
+        </Tags>
+      </div>
+    )
+  })
