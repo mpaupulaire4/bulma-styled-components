d3bf59a10265abaa25dc632c28766c812316b806
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-09-19 13:00:10 -0600

    update

--- catalog.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  webpack: (catalogWebpackConfig) => {
-    const modifiedWebpackConfig = catalogWebpackConfig
-    modifiedWebpackConfig.output.path = path.resolve('./docs')
-    return modifiedWebpackConfig
-  },
-}
