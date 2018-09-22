10a0f2b42cde2702b8efe755959cd5fb2c52acd3
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-09-22 19:30:55 -0400

    Notifi cation

--- src/elements/Notification.js
+++ src/elements/Notification.js
@@ -1,8 +1,9 @@
 /* stylelint-disable no-descending-specificity */
-import styled, { css } from 'styled-components'
+import React from 'react'
+import { css as emotion_css } from 'emotion'
+import { Consumer } from '../base'
 import Vars from '../utilities/vars'
 import { block } from '../utilities/mixins'
-import { fromTheme } from '../utilities/functions'
 import Button from './Button'
 import Delete from './Delete'
 import Title from './Title'
@@ -16,7 +17,7 @@ Vars.addDerivedDefault(vars => ({
   'notification-padding': '1.25rem 2.5rem 1.25rem 1.5rem',
 }))
 
-const colorClasses = props => Object.entries(props.theme.colors).reduce((acc, [name, [color, color_invert]]) => css`
+const colorClasses = theme => Object.entries(theme.colors).reduce((acc, [name, [color, color_invert]]) => emotion_css`
   ${acc}
   &.is-${name} {
     background-color: ${color};
@@ -24,13 +25,13 @@ const colorClasses = props => Object.entries(props.theme.colors).reduce((acc, [n
   }
 `, '')
 
-const Notification = styled.div`
+const NotificationStyle = theme => emotion_css`
   ${block}
-  background-color: ${fromTheme('notification-background-color')};
-  border-radius: ${fromTheme('notification-radius')};
-  padding: ${fromTheme('notification-padding')};
+  background-color: ${theme['notification-background-color']};
+  border-radius: ${theme['notification-radius']};
+  padding: ${theme['notification-padding']};
   position: relative;
-  a:not(${Button}):not(${DropdownItem}) { /* stylelint-disable-line */
+  a:not(.${Button.name}):not(.${DropdownItem.name}) { /* stylelint-disable-line */
     color: currentColor;
     text-decoration: underline;
   }
@@ -39,23 +40,42 @@ const Notification = styled.div`
   }
   code,
   pre {
-    background: ${fromTheme('white')};
+    background: ${theme['white']};
   }
   pre code {
     background: transparent;
   }
-  & > ${Delete} { /* stylelint-disable-line */
+  & > .${Delete.name} { /* stylelint-disable-line */
     position: absolute;
     right: 0.5rem;
     top: 0.5rem;
   }
-  ${/* sc-custom '.title' */Title},
-  ${/* sc-custom '.subtitle' */Subtitle},
-  ${/* sc-custom '.content' */Content} {
+  .${/* sc-custom '.title' */Title.name},
+  .${/* sc-custom '.subtitle' */Subtitle.name},
+  .${/* sc-custom '.content' */Content.name} {
     color: currentColor;
   }
-  ${colorClasses}
+  ${colorClasses(theme)}
 `
-Notification.defaultProps = { theme: Vars.getVariables() }
+export default class Notification extends React.PureComponent {
+  static defaultProps = {
+    as: 'div',
+    className: '',
+  }
 
-export default Notification
+  render() {
+    const { as, className, ...props } = this.props
+    return (
+      <Consumer>
+        {({ theme }) => React.createElement(as, {
+          ...props,
+          className: [
+            Notification.name,
+            NotificationStyle(theme, as),
+            className,
+          ].join(' '),
+        })}
+      </Consumer>
+    )
+  }
+}
