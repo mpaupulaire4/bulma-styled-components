84087a37ea01865f3ba37a818e687619f22a3c93
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-09-23 09:27:10 -0600

    var update

--- src/utilities/vars.js
+++ src/utilities/vars.js
@@ -4,19 +4,29 @@ export default class VarCalculator {
   static _variables = {}
   static _defaults = []
   static _derived = []
+  static _recalc = false
 
-  static getVariables = overrides => VarCalculator._derived.reduce((vars, der) => ({
-    ...(der(vars)),
-    ...vars,
-  }), getVariables(overrides))
-
-  static addDefault(obj) {
-    VarCalculator._defaults.push(obj)
+  static getVariables = (overrides) => {
+    if (this._overrides === overrides && !this._recalc) {
+      return this._variables
+    }
+    this._variables = VarCalculator._derived.reduce((vars, der) => ({
+      ...(der(vars)),
+      ...vars,
+    }), getVariables(overrides))
+    this._variables = VarCalculator._defaults.reduce((vars, der) => ({
+      ...der,
+      ...vars,
+    }), this._variables)
+    return this._variables
   }
 
   static addDerivedDefault(value) {
+    this._recalc = true
     if (typeof value === 'function') {
       VarCalculator._derived.push(value)
+    } else {
+      VarCalculator._defaults.push(value)
     }
   }
 }
