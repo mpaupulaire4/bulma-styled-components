0c803fea1bb7063e64750ef43393028c517d3163
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-09-23 19:36:05 -0600

    Field Control Checkbox

--- src/form/Control.js
+++ src/form/Control.js
@@ -1,86 +1,91 @@
 /* stylelint-disable no-descending-specificity */
-import styled from 'styled-components'
+import { css } from 'emotion'
 import Icon from '../elements/Icon'
-import { Select } from './Select'
-import { Input } from './Input'
+import Select from './Select'
+import Input from './Input'
 import Vars from '../utilities/vars'
 import { loader } from '../utilities/mixins'
-import { fromTheme } from '../utilities/functions'
+import { BaseWithConsumer } from '../base/Class'
 
 Vars.addDerivedDefault(vars => ({
   'input-icon-color': vars['grey-lighter'],
   'input-icon-active-color': vars['grey'],
 }))
 
-export const Control = styled.div`
-  clear: both; /* fixes the icon floating out of the input when help text is floated right */
-  font-size: ${fromTheme('size-normal')};
-  position: relative;
-  text-align: left;
-  /* Modifiers */
-  &.has-icons-left,
-  &.has-icons-right {
-    ${/* sc-custom '.icon' */Icon} {
-      color: ${fromTheme('input-icon-color')};
-      height: 2.25em;
-      pointer-events: none;
-      position: absolute;
-      top: 0;
-      width: 2.25em;
-      z-index: 4;
-    }
-    ${/* sc-custom '.input' */Input}:focus ~ ${/* sc-custom '.icon' */Icon},
-    ${/* sc-custom '.select' */Select}:focus ~ ${/* sc-custom '.icon' */Icon} {
-      color: ${fromTheme('input-icon-active-color')};
-    }
-    ${/* sc-custom '.input' */Input}:focus.is-mall ~ ${/* sc-custom '.icon' */Icon},
-    ${/* sc-custom '.select' */Select}:focus.is-small ~ ${/* sc-custom '.icon' */Icon} {
-      font-size: ${fromTheme('size-small')};
-    }
-    ${/* sc-custom '.input' */Input}:focus.is-medium ~ ${/* sc-custom '.icon' */Icon},
-    ${/* sc-custom '.select' */Select}:focus.is-smedium ~ ${/* sc-custom '.icon' */Icon} {
-      font-size: ${fromTheme('size-medium')};
-    }
-    ${/* sc-custom '.input' */Input}:focus.is-large ~ ${/* sc-custom '.icon' */Icon},
-    ${/* sc-custom '.select' */Select}:focus.is-large ~ ${/* sc-custom '.icon' */Icon} {
-      font-size: ${fromTheme('size-large')};
-    }
+export default class Control extends BaseWithConsumer {
+  static defaultProps = {
+    as: 'div',
   }
-  &.has-icons-left {
-    ${/* sc-custom '.input' */Input},
-    ${/* sc-custom '.select' */Select} select {
-      padding-left: 2.25em;
+
+  static Style = theme => css`
+    clear: both; /* fixes the icon floating out of the input when help text is floated right */
+    font-size: ${theme['size-normal']};
+    position: relative;
+    text-align: left;
+    /* Modifiers */
+    &.has-icons-left,
+    &.has-icons-right {
+      .${/* sc-custom '.icon' */Icon.name} {
+        color: ${theme['input-icon-color']};
+        height: 2.25em;
+        pointer-events: none;
+        position: absolute;
+        top: 0;
+        width: 2.25em;
+        z-index: 4;
+      }
+      .${/* sc-custom '.input' */Input.name}:focus ~ .${/* sc-custom '.icon' */Icon.name},
+      .${/* sc-custom '.select' */Select.name}:focus ~ .${/* sc-custom '.icon' */Icon.name} {
+        color: ${theme['input-icon-active-color']};
+      }
+      .${/* sc-custom '.input' */Input.name}:focus.is-mall ~ .${/* sc-custom '.icon' */Icon.name},
+      .${/* sc-custom '.select' */Select.name}:focus.is-small ~ ${/* sc-custom '.icon' */Icon.name} {
+        font-size: ${theme['size-small']};
+      }
+      .${/* sc-custom '.input' */Input.name}:focus.is-medium ~ .${/* sc-custom '.icon' */Icon.name},
+      .${/* sc-custom '.select' */Select.name}:focus.is-smedium ~ .${/* sc-custom '.icon' */Icon.name} {
+        font-size: ${theme['size-medium']};
+      }
+      .${/* sc-custom '.input' */Input.name}:focus.is-large ~ .${/* sc-custom '.icon' */Icon.name},
+      .${/* sc-custom '.select' */Select.name}:focus.is-large ~ ${/* sc-custom '.icon' */Icon.name} {
+        font-size: ${theme['size-large']};
+      }
     }
-    ${/* sc-custom '.icon' */Icon}.is-left {
-      left: 0;
+    &.has-icons-left {
+      .${/* sc-custom '.input' */Input.name},
+      .${/* sc-custom '.select' */Select.name} select {
+        padding-left: 2.25em;
+      }
+      .${/* sc-custom '.icon' */Icon.name}.is-left {
+        left: 0;
+      }
     }
-  }
-  &.has-icons-right {
-    ${/* sc-custom '.input' */Input},
-    ${/* sc-custom '.select' */Select} select {
-      padding-right: 2.25em;
+    &.has-icons-right {
+      .${/* sc-custom '.input' */Input.name},
+      .${/* sc-custom '.select' */Select.name} select {
+        padding-right: 2.25em;
+      }
+      .${/* sc-custom '.icon' */Icon.name}.is-right {
+        right: 0;
+      }
     }
-    ${/* sc-custom '.icon' */Icon}.is-right {
-      right: 0;
+    &.is-loading {
+      &::after {
+        ${loader(theme)}
+        position: absolute !important;
+        right: 0.625em;
+        top: 0.625em;
+        z-index: 4;
+      }
+      &.is-small:after {
+        font-size: ${theme['size-small']};
+      }
+      &.is-medium:after {
+        font-size: ${theme['size-medium']};
+      }
+      &.is-large:after {
+        font-size: ${theme['size-large']};
+      }
     }
-  }
-  &.is-loading {
-    &::after {
-      ${loader}
-      position: absolute !important;
-      right: 0.625em;
-      top: 0.625em;
-      z-index: 4;
-    }
-    &.is-small:after {
-      font-size: ${fromTheme('size-small')};
-    }
-    &.is-medium:after {
-      font-size: ${fromTheme('size-medium')};
-    }
-    &.is-large:after {
-      font-size: ${fromTheme('size-large')};
-    }
-  }
-`
-Control.defaultProps = { theme: Vars.getVariables() }
+  `
+}
