80322a2797eb5aed6572a01a3840e8dcdb8419d0
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-09-24 10:09:15 -0600

    Help

--- src/form/Help.js
+++ src/form/Help.js
@@ -1,22 +1,27 @@
-import styled, { css } from 'styled-components'
+import { css as emotion_css } from 'emotion'
 import Vars from '../utilities/vars'
-import { fromTheme } from '../utilities/functions'
+import { BaseWithConsumer } from '../base/Class'
 
 Vars.addDerivedDefault(vars => ({
   'help-size': vars['size-small'],
 }))
 
-const HelpColorClasses = ({ theme }) => Object.entries(theme['colors']).reduce((acc, [name, [color]]) => css`
+const HelpColorClasses = theme => Object.entries(theme['colors']).reduce((acc, [name, [color]]) => emotion_css`
   ${acc}
   &.is-${name} {
     color: ${color};
   }
 `, '')
 
-export const Help = styled.p`
-  display: block;
-  font-size: ${fromTheme('help-size')};
-  margin-top: 0.25rem;
-  ${HelpColorClasses}
-`
-Help.defaultProps = { theme: Vars.getVariables() }
+export default class Help extends BaseWithConsumer {
+  static defaultProps = {
+    as: 'p',
+  }
+
+  static Style = theme => emotion_css`
+    display: block;
+    font-size: ${theme['help-size']};
+    margin-top: 0.25rem;
+    ${HelpColorClasses(theme)}
+  `
+}
