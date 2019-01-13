939a2404648dadd5eebaecc101046c6c467a341a
Michel Paupulaire <mpaupulaire4@gmail.com>
2019-01-13 15:09:03 -0700

    export

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,6 @@ import { PureComponent, HTMLProps, Component } from 'react'
 
 declare module 'bulma-styled-components' {
   type Props = HTMLProps & { as: string | Component }
-  interface Base extends PureComponent<Props> {}
+  export interface Base extends PureComponent<Props> {}
 
 }
